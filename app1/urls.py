9521de17c8944c77a3240f895155befad03b800a
Deepti <deeptibhujel18@gmail.com>
2024-10-04 10:58:31 +0545

    Initial commit

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("hello/",views.hello),
-    path("hi/",views.hi)
+    # path("hi/",views.hi),
+    path('create/',views.create_student)
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,31 +3,40 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from . models import Student 
+from .forms import StudentForm
 
 def hello(request):
     students = Student.objects.all()
-    context = {
-        "S": students            #S is key
-    }
+    context ={"S": students}            #S is key 
     return render (request, "app1/index.html",context)
 
-def hi (request):
-    context = {
-        "user_info":{
-            "name":"Deepti",
-            "age": 18,
-            "location":"chhorepatan",
-        },
-        "hobbies":{
-            "indoor":["reading","coding","gaming"],
-            "outdoor":["hiking","cycling","swimming"],
-        },
-        "greeting":{
-            "message":"Hello,Welcome to my profile!",
-            "timestamp":"2081-06-07 10:57 am",
-        },
+def create_student(request):
+    form = StudentForm
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {"form": form}
+    
+    return render(request, 'app1/create_student.html',context)
+
+# def hi (request):
+#     context = {
+#         "user_info":{
+#             "name":"Deepti",
+#             "age": 18,
+#             "location":"chhorepatan",
+#         },
+#         "hobbies":{
+#             "indoor":["reading","coding","gaming"],
+#             "outdoor":["hiking","cycling","swimming"],
+#         },
+#         "greeting":{
+#             "message":"Hello,Welcome to my profile!",
+#             "timestamp":"2081-06-07 10:57 am",
+#         },
         
-    }
+#     }
         
     
-    return render(request, "app1/index.html", context)
+#     return render(request, "app1/index.html", context)
